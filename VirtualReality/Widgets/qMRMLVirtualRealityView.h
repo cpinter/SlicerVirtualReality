bfe91e8a780f8ff11ead4c00a582389b3ae0a9ae
Csaba Pinter <csaba.pinter@queensu.ca>
2019-06-14 10:20:55 -0400

    STYLE: Add function documentation

--- VirtualReality/Widgets/qMRMLVirtualRealityView.h
+++ VirtualReality/Widgets/qMRMLVirtualRealityView.h
@@ -31,7 +31,7 @@
 #include "qSlicerVirtualRealityModuleWidgetsExport.h"
 
 // CTK includes
-#include <ctkVTKObject.h> 
+#include <ctkVTKObject.h>
 
 class qMRMLVirtualRealityHomeWidget;
 class qMRMLVirtualRealityViewPrivate;
@@ -85,6 +85,7 @@ public:
   /// Get the 3D View node observed by view.
   Q_INVOKABLE vtkMRMLVirtualRealityViewNode* mrmlVirtualRealityViewNode()const;
 
+  /// Get VR home widget
   Q_INVOKABLE qMRMLVirtualRealityHomeWidget* vrHomeWidget()const;
 
   /// Get a reference to the associated vtkRenderer
@@ -130,6 +131,8 @@ public:
   Q_INVOKABLE QString actionManifestPath() const;
   ///@}
 
+  /// Register VR module. A button will be added in the modules section of the VR home
+  /// widget with the provided icon. When clicked, the given widget will be shown
   void registerModule(QWidget* widget, QIcon& icon);
 
 signals:
